641f090812fa4328d1dec5510f8410c9f8d6b2d5
Patrick Davlin <patrick@davlin.io>
2019-12-08 17:55:35 -0600

    aotd integrations

--- features/admin.js
+++ features/admin.js
@@ -1,7 +1,8 @@
-module.exports = function(controller) {
+module.exports = function (controller) {
   let channels;
   const channelLoader = async bot => {
-    bot.api.conversations.list({}, (err, response) => {
+    console.log('Attempting to load channels...');
+    await bot.api.conversations.list({}, (err, response) => {
       channels = response.channels;
     });
   };
@@ -9,6 +10,10 @@ module.exports = function(controller) {
   controller.hears('!say', 'direct_message', async (bot, message) => {
     if (channels === undefined) {
       console.log('channels undefined');
+      await bot.reply(
+        message,
+        `I'm trying to load the channel list.`
+      );
       await channelLoader(bot);
     } else if (channels === null) {
       console.log('channnels null');
@@ -16,30 +21,40 @@ module.exports = function(controller) {
     if (message.user === 'U1CRB864E') {
       let [inputChannel, ...rest] = message.text.split(' ').splice(1),
         text = rest.join(' '),
-        sayChannel;
-      console.log(`searching for ${inputChannel}`);
-      if (inputChannel.startsWith('#')) inputChannel.replace('#','');
-      for (const channel of channels) {
-        
-        if (channel.name.toUpperCase() === inputChannel.toUpperCase()) {
-          sayChannel = channel;
-          console.log(
-            `I found a channel called ${channel.name}, its id is ${channel.id}`
-          );
-          break;
+        channelId;
+      if (inputChannel.startsWith('<')) {
+        channelId = inputChannel.substring(
+          inputChannel.lastIndexOf("#") + 1,
+          inputChannel.lastIndexOf("|")
+        );
+      }
+      if (channels !== undefined && channelId === undefined) {
+        for (const channel of channels) {
+          if (channel.name.toUpperCase() === inputChannel.toUpperCase()) {
+            console.log(
+              `I found a channel called ${channel.name}, its id is ${channel.id}`
+            );
+            channelId = channel.id;
+            break;
+          }
         }
       }
-      if (!!sayChannel) {
+      if (!!channelId) {
         await bot.say({
           text,
-          channel: sayChannel.id
+          channel: channelId
         });
       } else {
         await bot.reply(
           message,
-          `channel not found!`
+          `I can't find that channel. Have you tried not being a lunkhead?`
         );
       }
+    } else {
+      await bot.reply(
+        message,
+        `Don't tell me what to do, I'm not your lackey`
+      );
     }
   });
 };
--- features/aotd.js
+++ features/aotd.js
@@ -1,90 +1,99 @@
 const request = require('request-promise');
-const { SlackDialog } = require('botbuilder-adapter-slack');
+const MongoClient = require('mongodb').MongoClient;
+const url = `${process.env.MONGO_URI}`;
 
-module.exports = function (controller) {
-    controller.hears(['musiclink', new RegExp(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/, 'i')], 'message', async function (bot, message) {
-        let songlinkReply;
-        if (message.matches[0].includes('spotify') && (message.matches[4].includes('album') || message.matches[4].includes('track'))) {
-            let inputArray = message.matches[4].split('/');
-            inputArray[2] = inputArray[2].split('?')[0];
-            if (inputArray[1] === 'track')
-                inputArray[1] = 'song';
-            
-            try {
-                songlinkReply = await songlinkRequestByTypeAndId(inputArray[1], inputArray[2]);
+const songlinkRequestByTypeAndId = async (inputType, inputId) =>
+  request({
+    uri: 'https://api.song.link/v1-alpha.1/links',
+    method: 'GET',
+    qs: {
+      userCountry: 'US',
+      type: inputType,
+      platform: 'spotify',
+      id: inputId
+    },
+    json: true
+  })
 
-            } catch (err) {
-                songlinkReply = '';
-                console.error(err);
-            }
-        } else if (message.matches[0].includes('play.google.com')) {
-            try {
-                songlinkReply = await songlinkRequestByUrl(encodeURI(message.matches[0]));
-            } catch (err) {
-                songlinkReply = '';
-                console.error(err);
-            }
-        }
+const songlinkRequestByUrl = async url =>
+  request({
+    uri: 'https://api.song.link/v1-alpha.1/links',
+    method: 'GET',
+    qs: {
+      url: url
+    },
+    json: true
+  })
 
-        if (songlinkReply !== '') {
-            const songlinkEntityId = songlinkReply.entityUniqueId;
-            await bot.reply(message, {
-                blocks: [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": `Looks like a great ${songlinkReply.entitiesByUniqueId[songlinkEntityId].type}! *Here's a link that everyone can use to listen:*`
-                        }
-                    },
-                    {
-                        "type": "divider"
-                    },
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": `\n*Artist:* ${songlinkReply.entitiesByUniqueId[songlinkEntityId].artistName}\n*Album*: ${songlinkReply.entitiesByUniqueId[songlinkEntityId].title}\n*SongLink URL*: ${songlinkReply.pageUrl}`
-                        },
-                        "accessory": {
-                            "type": "image",
-                            "image_url": songlinkReply.entitiesByUniqueId[songlinkEntityId].thumbnailUrl,
-                            "alt_text": "alt text for image"
-                        }
-                    }
-                ]
-            });
-        }
-    });
+const checkIfAlbumInDb = (id) => {
+  MongoClient.connect(url, async (err, client) => {
+    const db = client.db("aotd");
+    db.collection("upcoming").find({ id: id }).toArray().then((result) => {
+      if (result.length === 0) return true;
+      else return false;
+
+    }).catch((err) => {
+
+      console.error(err);
+      return false;
+    }).finally(() => db.close());
+  });
 }
 
-async function songlinkRequestByTypeAndId(inputType, inputId) {
-    console.log(inputId);
-    const options = {
-        uri: 'https://api.song.link/v1-alpha.1/links',
-        method: 'GET',
-        qs: {
-            userCountry: 'US',
-            type: inputType,
-            platform: 'spotify',
-            id: inputId
-        },
-        json: true
-    }
-    result = request(options);
-    return result;
-}
+module.exports = (controller) => {
+  controller.hears(['musiclink', new RegExp(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/, 'i')], 'message', async function (bot, message) {
+    let songlinkReply;
+    if (message.matches[0].includes('spotify') && (message.matches[4].includes('album') || message.matches[4].includes('track'))) {
+      let inputArray = message.matches[4].split('/');
+      inputArray[2] = inputArray[2].split('?')[0];
+      if (inputArray[1] === 'track')
+        inputArray[1] = 'song';
 
-async function songlinkRequestByUrl(url) {
-    console.log(url);
-    const options = {
-        uri: 'https://api.song.link/v1-alpha.1/links',
-        method: 'GET',
-        qs: {
-            url: url
-        },
-        json: true
+      try {
+        songlinkReply = await songlinkRequestByTypeAndId(inputArray[1], inputArray[2]);
+
+      } catch (err) {
+        songlinkReply = '';
+        console.error(err);
+      }
+    } else if (message.matches[0].includes('play.google.com')) {
+      try {
+        songlinkReply = await songlinkRequestByUrl(encodeURI(message.matches[0]));
+      } catch (err) {
+        songlinkReply = '';
+        console.error(err);
+      }
     }
-    result = request(options);
-    return result;
+
+    if (songlinkReply !== '') {
+      const songlinkEntityId = songlinkReply.entityUniqueId;
+      await bot.reply(message, {
+        blocks: [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": `Looks like a great ${songlinkReply.entitiesByUniqueId[songlinkEntityId].type}! *Here's a link that everyone can use to listen:*`
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": `\n*Artist:* ${songlinkReply.entitiesByUniqueId[songlinkEntityId].artistName}\n*Album*: ${songlinkReply.entitiesByUniqueId[songlinkEntityId].title}\n*SongLink URL*: ${songlinkReply.pageUrl}`
+            },
+            "accessory": {
+              "type": "image",
+              "image_url": songlinkReply.entitiesByUniqueId[songlinkEntityId].thumbnailUrl,
+              "alt_text": "alt text for image"
+            }
+          }
+        ]
+      });
+      if (songlinkReply.entitiesByUniqueId[songlinkEntityId].type === 'album' && !checkIfAlbumInDb(songlinkEntityId)) askWhetherToStoreAlbumInDb()
+    }
+  });
 }
